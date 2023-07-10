78a752fffb705d7fd59262251de64d835bb8f263
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-10 04:15:48 +0300

    add primary & secondary colors

--- app/src/main/java/com/shaban/donuts/ui/theme/Color.kt
+++ app/src/main/java/com/shaban/donuts/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.shaban.donuts.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFFFF7074)
+val Secondary = Color(0xFFFF9494)
+val BackgroundColor = Color(0xFFFED8DF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray = Color(0xFF585858)
