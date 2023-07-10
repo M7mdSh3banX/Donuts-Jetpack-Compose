32a0832ba6a29896aacf70c9016f00cd2b534bf8
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-10 04:33:55 +0300

    add typography using inter font family

---
+++ app/src/main/java/com/shaban/donuts/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.shaban.donuts.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.shaban.donuts.R
+
+val Inter = FontFamily(
+    Font(resId = R.font.inter_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.inter_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.inter_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.inter_bold, weight = FontWeight.Bold)
+)
--- app/src/main/java/com/shaban/donuts/ui/theme/Type.kt
+++ app/src/main/java/com/shaban/donuts/ui/theme/Type.kt
@@ -2,33 +2,59 @@ package com.shaban.donuts.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    // region Title
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = Inter,
+        fontWeight = FontWeight.Bold,
+        fontSize = 54.sp,
+        color = Primary
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp,
+        color = Primary
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        color = Primary
+    ),
+    // endregion
+
+    // region Body
+    bodyLarge = TextStyle(
+        fontFamily = Inter,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 16.sp,
+        color = Black
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        color = Black
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        color = Black
+    ),
+    // endregion
+
+    // region Label
+    labelLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        color = Black
     )
-    */
+    // endregion
 )
