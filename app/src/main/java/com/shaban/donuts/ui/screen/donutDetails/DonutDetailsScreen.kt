373ece4bc45d0795777ae26ae2e16a73b8f1303a
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-10 23:00:58 +0300

    make fav icon between image and details

--- app/src/main/java/com/shaban/donuts/ui/screen/donutDetails/DonutDetailsScreen.kt
+++ app/src/main/java/com/shaban/donuts/ui/screen/donutDetails/DonutDetailsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -19,7 +20,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -71,135 +71,145 @@ fun DonutDetailsContent(
             .fillMaxSize()
             .background(color = BackgroundColor)
     ) {
-        Column(
+        Box(
             modifier = Modifier
-                .padding(top = 16.dp)
                 .weight(1F)
-                .fillMaxSize()
+                .fillMaxWidth()
         ) {
+            Column(
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxSize()
+            ) {
+                IconButton(
+                    modifier = Modifier
+                        .padding(top = 16.dp, start = 16.dp)
+                        .size(40.dp),
+                    onClick = { }
+                ) {
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(id = R.drawable.ic_arrow_back),
+                        contentDescription = "Arrow back Icon",
+                        tint = Primary
+                    )
+                }
+
+                Image(
+                    painter = painterResource(id = R.drawable.strawberry_wheel_donut),
+                    contentDescription = state.name,
+                    modifier = Modifier
+                        .aspectRatio(1F)
+                        .align(Alignment.CenterHorizontally),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .weight(1F)
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+                    .background(color = WhiteBackground)
+                    .padding(40.dp)
+            ) {
+                Text(text = "Strawberry Wheel", style = Typography.titleMedium)
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "About Donut", style = Typography.bodyLarge)
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = state.description,
+                    style = Typography.bodySmall
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Quantity", style = Typography.bodyLarge)
+                Row(
+                    modifier = Modifier.padding(top = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    IconButton(
+                        modifier = Modifier
+                            .padding(end = 20.dp)
+                            .background(color = White, shape = RoundedCornerShape(8.dp))
+                            .size(40.dp),
+                        onClick = { if (state.quantity > 1) onClickDecreaseQuantity(state) }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(16.dp),
+                            painter = painterResource(id = R.drawable.ic_decrease),
+                            contentDescription = "Arrow back Icon",
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .background(color = White, shape = RoundedCornerShape(8.dp))
+                            .size(40.dp)
+                    ) {
+                        Text(
+                            text = state.quantity.toString(),
+                            style = Typography.bodyLarge,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                    IconButton(
+                        modifier = Modifier
+                            .padding(start = 20.dp)
+                            .background(color = Black, shape = RoundedCornerShape(8.dp))
+                            .size(40.dp),
+                        onClick = { onClickIncreaseQuantity(state) }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(16.dp),
+                            painter = painterResource(id = R.drawable.ic_increase),
+                            contentDescription = "Arrow back Icon",
+                            tint = White
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.weight(1F))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "£${state.totalPrice}",
+                        style = Typography.titleMedium.copy(color = Black),
+                    )
+                    Spacer(modifier = Modifier.width(24.dp))
+                    Button(
+                        modifier = Modifier.height(56.dp),
+                        onClick = { },
+                        colors = ButtonDefaults.buttonColors(Secondary),
+                    ) {
+                        Text(
+                            text = "Add to Cart",
+                            color = White,
+                            style = Typography.labelLarge
+                        )
+                    }
+                }
+            }
             IconButton(
                 modifier = Modifier
-                    .padding(top = 16.dp, start = 16.dp)
-                    .size(40.dp),
-                onClick = { }
+                    .offset(y = (-24).dp)
+                    .padding(end = 16.dp)
+                    .size(45.dp)
+                    .background(color = White, shape = RoundedCornerShape(10.dp))
+                    .align(alignment = Alignment.TopEnd),
+                onClick = { onClickFavoriteIcon(state) }
             ) {
                 Icon(
                     modifier = Modifier.size(24.dp),
-                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    painter = painterResource(id = if (state.isFavorite) R.drawable.ic_favorite_filled else R.drawable.ic_favorite),
                     contentDescription = "Arrow back Icon",
                     tint = Primary
                 )
-            }
-
-            Image(
-                painter = painterResource(id = R.drawable.strawberry_wheel_donut),
-                contentDescription = state.name,
-                modifier = Modifier
-                    .aspectRatio(1F)
-                    .align(Alignment.CenterHorizontally),
-                contentScale = ContentScale.Crop
-            )
-        }
-        IconButton(
-            modifier = Modifier
-                .padding(16.dp)
-                .size(45.dp)
-                .background(color = White, shape = RoundedCornerShape(10.dp))
-                .align(alignment = Alignment.End),
-            onClick = { onClickFavoriteIcon(state) }
-        ) {
-            Icon(
-                modifier = Modifier.size(24.dp),
-                painter = painterResource(id = if (state.isFavorite) R.drawable.ic_favorite_filled else R.drawable.ic_favorite),
-                contentDescription = "Arrow back Icon",
-                tint = Primary
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .weight(1F)
-                .clip(RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
-                .background(color = WhiteBackground)
-                .padding(40.dp)
-        ) {
-            Text(text = "Strawberry Wheel", style = Typography.titleMedium)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "About Donut", style = Typography.bodyLarge)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = state.description,
-                style = Typography.bodySmall
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "Quantity", style = Typography.bodyLarge)
-            Row(
-                modifier = Modifier.padding(top = 16.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                IconButton(
-                    modifier = Modifier
-                        .padding(end = 20.dp)
-                        .background(color = White, shape = RoundedCornerShape(8.dp))
-                        .size(40.dp),
-                    onClick = { if (state.quantity > 1) onClickDecreaseQuantity(state) }
-                ) {
-                    Icon(
-                        modifier = Modifier.size(16.dp),
-                        painter = painterResource(id = R.drawable.ic_decrease),
-                        contentDescription = "Arrow back Icon",
-                    )
-                }
-                Box(
-                    modifier = Modifier
-                        .background(color = White, shape = RoundedCornerShape(8.dp))
-                        .size(40.dp)
-                ) {
-                    Text(
-                        text = state.quantity.toString(),
-                        style = Typography.bodyLarge,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-                IconButton(
-                    modifier = Modifier
-                        .padding(start = 20.dp)
-                        .background(color = Black, shape = RoundedCornerShape(8.dp))
-                        .size(40.dp),
-                    onClick = { onClickIncreaseQuantity(state) }
-                ) {
-                    Icon(
-                        modifier = Modifier.size(16.dp),
-                        painter = painterResource(id = R.drawable.ic_increase),
-                        contentDescription = "Arrow back Icon",
-                        tint = White
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Spacer(modifier = Modifier.weight(1F))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = "£${state.totalPrice}",
-                    style = Typography.titleMedium.copy(color = Black),
-                )
-                Spacer(modifier = Modifier.width(24.dp))
-                Button(
-                    modifier = Modifier.height(56.dp),
-                    onClick = { },
-                    colors = ButtonDefaults.buttonColors(Secondary),
-                ) {
-                    Text(
-                        text = "Add to Cart",
-                        color = White,
-                        style = Typography.labelLarge
-                    )
-                }
             }
         }
     }
