767055fc30a4b3452481f96e32117ebdb4da95f5
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-10 05:02:07 +0300

    add BabyBlue and WhiteFA colors

--- app/src/main/java/com/shaban/donuts/ui/theme/Color.kt
+++ app/src/main/java/com/shaban/donuts/ui/theme/Color.kt
@@ -5,7 +5,9 @@ import androidx.compose.ui.graphics.Color
 val Primary = Color(0xFFFF7074)
 val Secondary = Color(0xFFFF9494)
 val BackgroundColor = Color(0xFFFED8DF)
+val BabyBlue = Color(0xFFD7E4F6)
 
 val White = Color(0xFFFFFFFF)
+val WhiteFA = Color(0xFFFAFAFA)
 val Black = Color(0xFF000000)
 val Gray = Color(0xFF585858)
