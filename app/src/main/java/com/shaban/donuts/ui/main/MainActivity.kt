6e58f9d9b447f4d1389bf664dc4f3fe16f830f2a
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-15 06:07:56 +0300

    remove unused imports

--- app/src/main/java/com/shaban/donuts/ui/main/MainActivity.kt
+++ app/src/main/java/com/shaban/donuts/ui/main/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
-import com.shaban.donuts.ui.screen.donutDetails.DonutDetailsScreen
-import com.shaban.donuts.ui.screen.home.HomeScreen
-import com.shaban.donuts.ui.screen.onboarding.OnboardingScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
